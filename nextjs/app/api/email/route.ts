950693ee9f4b763f8992697f9975a09355f19da2
Gabriel Garza <gabe84700@gmail.com>
2024-10-06 23:15:40 -0500

    Update route.ts

--- nextjs/app/api/email/route.ts
+++ nextjs/app/api/email/route.ts
@@ -5,6 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
+//test
 
 export async function POST(request: NextRequest) {
   try {
